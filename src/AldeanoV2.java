70fddecd5e3f66348148d8d210b9bd544aa5fa61
IvoBiaus <43274372+IvoBiaus@users.noreply.github.com>
2018-11-10 22:56:28 -0300

    Add files via upload

---
+++ src/AldeanoV2.java
@@ -0,0 +1,24 @@
+
+public class Aldeano extends Unidad {
+	
+	Aldeano(Casilla unaCasilla) {
+		super(unaCasilla);
+		vida = 50;
+		costo = 25;
+	}
+
+	public void reparar() {
+		
+	}
+	
+	/* public void generarOro(){	//hago una funcion o multiplico por cantidad de aldeanos libres?
+	 } */
+	public void crearPlaza() {
+		
+	}
+	
+	public void crearCuartel() {
+		
+	}
+}
+
