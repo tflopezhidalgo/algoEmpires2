fb9ec7206670a38313860ebf86195e990c963a19
IvoBiaus <43274372+IvoBiaus@users.noreply.github.com>
2018-11-11 03:30:41 -0300

    Add files via upload

---
+++ test/TestCastilloCreaUnidad.java
@@ -0,0 +1,47 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+class TestCastilloCreaUnidad {
+
+	@Test
+	void castilloCreaCatapulta() {
+		
+		
+		Casilla miniTablero[][] = new Casilla[6][6];
+		for(int y = 0; y < 6; y++ ) {
+			for(int x = 0; x < 6; x++ ) {
+				Posicion unaPosicion = new Posicion(x,y);
+				Casilla casilla = new Casilla(unaPosicion);
+				
+				miniTablero[x][y] = casilla;	
+			}
+		}
+		
+		
+		List<Casilla> casillasParaConstruccion = new ArrayList<Casilla>();
+		//voy a ocupar a partir de la (0,0)
+		for(int y = 0; y < Castillo.TAMANIO_LADO; y++) {
+			for(int x = 0; x < Castillo.TAMANIO_LADO; x++) {
+				casillasParaConstruccion.add(miniTablero[x][y]);
+			}
+		}
+		Area zonaDeConstruccion = new Area(casillasParaConstruccion);
+		
+		
+		Castillo unCastillo = new Castillo(zonaDeConstruccion);
+		assertEquals(true, miniTablero[3][3].estaOcupada());
+		
+		Casilla casillaParaCatapulta = miniTablero[4][4];
+		assertEquals(false, casillaParaCatapulta.estaOcupada());
+		
+		Catapulta nuevaCatapulta = unCastillo.crearCatapulta(casillaParaCatapulta);
+		assertEquals(true, casillaParaCatapulta.estaOcupada());
+		assertEquals(casillaParaCatapulta, nuevaCatapulta.obtenerUbicacion());
+
+	}
+}
+
