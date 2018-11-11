5449507cd373a4fe815d63694be07bda056289d3
IvoBiaus <43274372+IvoBiaus@users.noreply.github.com>
2018-11-10 22:55:03 -0300

    Update Unidad.java

--- src/Unidad.java
+++ src/Unidad.java
@@ -18,4 +18,49 @@ public abstract class Unidad extends Pieza {
 		return casillaActual;
 	}
 	
+	
+	//----------PROTOTIPO-----------------
+	
+	public void moverArriba() {
+		Casilla nuevaCasilla = casillaActual.casillaArriba();
+		mover(nuevaCasilla);
+	}
+	
+	public void moverAbajo() {
+		Casilla nuevaCasilla = casillaActual.casillaAbajo();
+		mover(nuevaCasilla);
+	}
+	
+	public void moverIzquierda() {
+		Casilla nuevaCasilla = casillaActual.casillaIzquierda();
+		mover(nuevaCasilla);
+	}
+	
+	public void moverDerecha() {
+		Casilla nuevaCasilla = casillaActual.casillaDerecha();
+		mover(nuevaCasilla);
+	}
+	
+	//----------------DIAGONALES-------
+	
+	public void moverArribaIzquierda() {
+		Casilla nuevaCasilla = casillaActual.casillaArribaIzquiera();
+		mover(nuevaCasilla);
+	}
+	
+	public void moverArribaDerecha() {
+		Casilla nuevaCasilla = casillaActual.casillaArribaDerecha();
+		mover(nuevaCasilla);
+	}
+	
+	public void moverAbajoDerecha() {
+		Casilla nuevaCasilla = casillaActual.casillaAbajoDerecha();
+		mover(nuevaCasilla);
+	}
+	
+	public void moverAbajoIzquierda() {
+		Casilla nuevaCasilla = casillaActual.casillaAbajoIzquierda();
+		mover(nuevaCasilla);
+	}
+
 }
