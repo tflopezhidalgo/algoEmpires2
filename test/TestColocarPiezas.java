258fb4334a1d4ae1b6e8aa8cd31b3a13e5e421ee
IvoBiaus <43274372+IvoBiaus@users.noreply.github.com>
2018-11-10 23:25:00 -0300

    Update TestColocarPiezas.java

--- test/TestColocarPiezas.java
+++ test/TestColocarPiezas.java
@@ -21,7 +21,7 @@ class TestColocarPiezas {
 	}
 	
 	@Test
-	public void EdificioColocado() {
+	public void ColocarPlaza() {
 		
 		Casilla miniTablero[][] = new Casilla[5][5];
 		
@@ -37,6 +37,7 @@ class TestColocarPiezas {
 		List<Casilla> casillasParaConstruccion = new ArrayList<Casilla>();
 		
 		//voy a ocupar a partir de la (2,2)
+		//se ocuparan las casillas en (2,2) (2,3) (3,2) (3,3) 
 		for(int y = 0; y < Plaza.TAMANIO_LADO; y++) {
 			for(int x = 0; x < Plaza.TAMANIO_LADO; x++) {
 				casillasParaConstruccion.add(miniTablero[2+x][2+y]);
@@ -48,6 +49,88 @@ class TestColocarPiezas {
 		
 		Plaza unaPlaza = new Plaza(zonaDeConstruccion);
 		assertEquals(false, zonaDeConstruccion.estaLibre());
+		
+		//Las casillas adyacentes deberian estar libres
+		assertEquals(false, miniTablero[1][1].estaOcupada());
+		assertEquals(false, miniTablero[4][4].estaOcupada());
+		assertEquals(false, miniTablero[2][1].estaOcupada());
+		assertEquals(false, miniTablero[3][4].estaOcupada());
+	}
+	
+	@Test
+	public void ColocarCuartel() {
+		
+		Casilla miniTablero[][] = new Casilla[5][5];
+		
+		for(int y = 0; y < 5; y++ ) {
+			for(int x = 0; x < 5; x++ ) {
+				Posicion unaPosicion = new Posicion(x,y);
+				Casilla casilla = new Casilla(unaPosicion);
+				
+				miniTablero[x][y] = casilla;	
+			}
+		}
+		
+		List<Casilla> casillasParaConstruccion = new ArrayList<Casilla>();
+		
+		//voy a ocupar a partir de la (2,2)
+		//se ocuparan las casillas en (2,2) (2,3) (3,2) (3,3) 
+		for(int y = 0; y < Cuartel.TAMANIO_LADO; y++) {
+			for(int x = 0; x < Cuartel.TAMANIO_LADO; x++) {
+				casillasParaConstruccion.add(miniTablero[2+x][2+y]);
+			}
+		}
+		
+		Area zonaDeConstruccion = new Area(casillasParaConstruccion);
+		assertEquals(true, zonaDeConstruccion.estaLibre());
+		
+		Cuartel unCuartel = new Cuartel(zonaDeConstruccion);
+		assertEquals(false, zonaDeConstruccion.estaLibre());
+		
+		//Las casillas adyacentes deberian estar libres
+		assertEquals(false, miniTablero[1][1].estaOcupada());
+		assertEquals(false, miniTablero[4][4].estaOcupada());
+		assertEquals(false, miniTablero[2][1].estaOcupada());
+		assertEquals(false, miniTablero[3][4].estaOcupada());
+	}
+	
+	@Test
+	public void ColocarCastillo() {
+		
+		Casilla miniTablero[][] = new Casilla[5][5];
+		
+		for(int y = 0; y < 5; y++ ) {
+			for(int x = 0; x < 5; x++ ) {
+				Posicion unaPosicion = new Posicion(x,y);
+				Casilla casilla = new Casilla(unaPosicion);
+				
+				miniTablero[x][y] = casilla;	
+			}
+		}
+		
+		List<Casilla> casillasParaConstruccion = new ArrayList<Casilla>();
+		
+		//voy a ocupar a partir de la (1,1) + 3 casillas de alto y 3 de ancho
+		for(int y = 0; y < Castillo.TAMANIO_LADO; y++) {
+			for(int x = 0; x < Castillo.TAMANIO_LADO; x++) {
+				casillasParaConstruccion.add(miniTablero[1+x][1+y]);
+			}
+		}
+		
+		Area zonaDeConstruccion = new Area(casillasParaConstruccion);
+		assertEquals(true, zonaDeConstruccion.estaLibre());
+		
+		Castillo unCastillo = new Castillo(zonaDeConstruccion);
+		assertEquals(false, zonaDeConstruccion.estaLibre());
+		
+		
+		assertEquals(true, miniTablero[3][4].estaOcupada());
+		assertEquals(true, miniTablero[4][4].estaOcupada());
+		//Las casillas adyacentes deberian estar libres
+		assertEquals(false, miniTablero[0][0].estaOcupada());
+		assertEquals(false, miniTablero[0][2].estaOcupada());
+		assertEquals(false, miniTablero[0][3].estaOcupada());
+		assertEquals(false, miniTablero[0][4].estaOcupada());
 
 	}
 	
