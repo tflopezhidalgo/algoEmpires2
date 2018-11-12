6ab3893f27cb6dd0d294f96f41c00c06909e2da7
IvoBiaus <43274372+IvoBiaus@users.noreply.github.com>
2018-11-12 01:27:06 -0300

    Update Cuartel.java

--- src/Cuartel.java
+++ src/Cuartel.java
@@ -13,12 +13,22 @@ public class Cuartel extends Edificio {
 		areaAOcupar.contruir(this);
 	}
 
-	public Espadachin crearEspadachin(Casilla ubicacion) {
+	public Espadachin crearEspadachin(Casilla ubicacion) throws ErrorBasico {
+		if(ubicacion.estaOcupada()) {
+			//TODO error
+			throw new ErrorBasico("ERROR: La ubicacion para colocar al soldado esta ocupada.");
+		}
+		
 		Espadachin unEspadachin = new Espadachin(ubicacion);
 		return unEspadachin;
 	}
 	
-	public Arquero crearArquero(Casilla ubicacion) {
+	public Arquero crearArquero(Casilla ubicacion) throws ErrorBasico {
+		if(ubicacion.estaOcupada()) {
+			//TODO error
+			throw new ErrorBasico("ERROR: La ubicacion para colocar al soldado esta ocupada.");
+		}
+		
 		Arquero unArquero = new Arquero(ubicacion);
 		return unArquero;
 	}
