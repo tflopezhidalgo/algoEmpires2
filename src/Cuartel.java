205af6fe41ce7f9d345952a38baa123b1da41ed5
IvoBiaus <43274372+IvoBiaus@users.noreply.github.com>
2018-11-18 16:33:44 -0300

    Update Cuartel.java

--- src/Cuartel.java
+++ src/Cuartel.java
@@ -10,10 +10,10 @@ public class Cuartel extends Edificio {
 		cantidadDeCuracion = 50;
 		
 		casillasOcupadas = areaAOcupar;
-		areaAOcupar.construir(this);
+		areaAOcupar.ocupar();
 	}
 	
-/*	//TODO ELIMINAR ESTO
+	//TODO ELIMINAR ESTO - es solo para realizar en AldeanoTest "AldeanoVuelveASumarOroLuegoDeFinalizarUnaReparacion"
 	Cuartel(Area areaAOcupar, boolean yaConstruida) throws Excepcion {
 		vida = 250;
 		vidaMaxima = vida;
@@ -26,39 +26,26 @@ public class Cuartel extends Edificio {
 		
 		cantidadDeCuracion = 50;
 		
-		casillasOcupadas = areaAOcupar;
-		areaAOcupar.construir(this);
-	}
-*/
-	Cuartel(Area areaAOcupar, boolean yaConstruida) throws Excepcion {
-		vida = 250;
-		vidaMaxima = vida;
-		costo = 50;
-		tiempoDeConstruccion = 3;
-		if(yaConstruida){
-			tiempoDeConstruccion=0;
-		}
-		cantidadDeCuracion = 50;
-
-		casillasOcupadas = areaAOcupar;
-		areaAOcupar.construir(this);
+		espacioOcupado = areaAOcupar;
+		areaAOcupar.ocupar();
 	}
 
-	public Espadachin crearEspadachin(Casilla ubicacion) throws Excepcion {
-		if(ubicacion.estaOcupada()) {
+
+	public Espadachin crearEspadachin(Area unEspacio) throws Excepcion {
+		if(!unEspacio.estaLibre()) {
 			throw new Excepcion("ERROR: La ubicacion para colocar al soldado esta ocupada.");
 		}
 		
-		Espadachin unEspadachin = new Espadachin(ubicacion);
+		Espadachin unEspadachin = new Espadachin(unEspacio);
 		return unEspadachin;
 	}
 	
-	public Arquero crearArquero(Casilla ubicacion) throws Excepcion {
-		if(ubicacion.estaOcupada()) {
+	public Arquero crearArquero(Area unEspacio) throws Excepcion {
+		if(!unEspacio.estaLibre()) {
 			throw new Excepcion("ERROR: La ubicacion para colocar al soldado esta ocupada.");
 		}
 		
-		Arquero unArquero = new Arquero(ubicacion);
+		Arquero unArquero = new Arquero(unEspacio);
 		return unArquero;
 	}
 	
