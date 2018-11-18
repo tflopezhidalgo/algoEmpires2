af5cd16dfe95c54cab114c300ba6059f84945b40
IvoBiaus <43274372+IvoBiaus@users.noreply.github.com>
2018-11-18 16:16:32 -0300

    Update Pieza.java

--- src/Pieza.java
+++ src/Pieza.java
@@ -4,56 +4,41 @@ public abstract class Pieza {
 	
 	protected int vida;
 	protected int costo; //No sé si una pieza debería saber su costo
-    protected Area areaOcupada;
+    	protected Area areaOcupada;
 	protected boolean jugoEnTurno;
 
-	public Pieza(Area unArea) throws Excepcion{
-	    this.vida = 0;
-	    this.costo = 0;
-	    this.areaOcupada = unArea;
-	    this.areaOcupada.ocuparConPieza(this);
-	    this.jugoEnTurno = false;
-    }
-
 	public Area obtenerAreaOcupada(){
-
 	    return this.areaOcupada;
-    }
-
-    public void mover(Area unArea) throws Excepcion {
-
-        areaOcupada.liberarCasillas();
-        areaOcupada = unArea;
-        areaOcupada.ocuparConPieza(this);
-        // TODO: No tiene mucho sentido pasarle la pieza a la casilla/area si total no la conoce.
-    }
+    	}
 
 	public void recibirDanio(int danio) {
-
-		this.vida = this.vida - danio;
-
+		vida -= danio;
 		if(vida <= 0) {
-
 			vida = 0;
 			//TODO destruir/morir/perder
-			areaOcupada.liberarCasillas();
+			liberarUbicacion();
 		}
 	}
 
 	public void nuevoTurno() {
-
 		jugoEnTurno = false;
 	}
 	
 	public boolean estaDestruida() {
-
 		return (vida == 0);
 	}
+	
+	protected void liberarUbicacion() {
+		espacioOcupado.liberar();
+	}
+	
+	public Area espacioOcupado() {
+		return espacioOcupado;
+	}	
 
 	/*          Métodos protected.          */
     //TODO: Sacar estos métodos falopas.
     protected void siYaJugoElTurnoError() throws Excepcion {
-
         if(this.jugoEnTurno) {
 
             throw new Excepcion("ERROR: Turno ya jugado.");
@@ -61,13 +46,13 @@ public abstract class Pieza {
     }
 
     protected boolean enRango(Pieza pieza, int distanciaMaxima) throws Excepcion {
+	Area espacioEnemigo = pieza.espacioOcupado();
+	int distanciaMinima = distanciaMinimaA(espacioEnemigo);
 
-        int distanciaMinima = distanciaMinimaA(pieza.obtenerAreaOcupada());
-
-        if(distanciaMinima > distanciaMaxima)
-            return false;
-
-        return true;
+	if(distanciaMinima > distanciaMaxima) {
+		return false;
+	}
+	return true;
     }
 
     protected int distanciaMinimaA(Area area){
