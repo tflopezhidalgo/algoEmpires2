c63240f30094aa4d488c79a8d249de865d22a749
IvoBiaus <43274372+IvoBiaus@users.noreply.github.com>
2018-11-18 16:31:54 -0300

    Update Edificio.java

--- src/Edificio.java
+++ src/Edificio.java
@@ -4,17 +4,8 @@ public abstract class Edificio extends Pieza {
 	protected int tiempoDeConstruccion;
 	protected int cantidadDeCuracion;
 
-	public Edificio(Area unArea) throws Excepcion{
-	    super(unArea);
-	    this.vidaMaxima = 0;
-	    this.tiempoDeConstruccion = 0;
-	    this.cantidadDeCuracion = 0;
-    }
-
 	public void reparar() {
-
 		vida += cantidadDeCuracion;
-
 		if(vida >= vidaMaxima) {
 			vida = vidaMaxima;
 			// liberar al aldeano de su labor
@@ -22,12 +13,10 @@ public abstract class Edificio extends Pieza {
 	}
 	
 	public boolean necesitaReparacion() {
-
 		return(vida < vidaMaxima);
 	}
 	
 	public boolean enConstruccion() {
-
 		return(tiempoDeConstruccion > 0);
 	}
 	
@@ -35,6 +24,10 @@ public abstract class Edificio extends Pieza {
 		if(enConstruccion()) {
 			tiempoDeConstruccion --;
 		}
+	}
+	
+	public int tamanioArea() {
+		return espacioOcupado.obtenerTamanio();
 	}
 
 }
