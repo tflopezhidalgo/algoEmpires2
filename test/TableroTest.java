12b02e390c1581baf277f7e61e4a87e0df0caa50
IvoBiaus <43274372+IvoBiaus@users.noreply.github.com>
2018-11-18 19:51:21 -0300

    Update TableroTest.java

--- test/TableroTest.java
+++ test/TableroTest.java
@@ -1,5 +1,5 @@
 import static org.junit.Assert.*;
-
+import modelo.*;
 import org.junit.Assert;
 import org.junit.Rule;
 import org.junit.Test;
@@ -11,37 +11,37 @@ public class TableroTest {
 	public ExpectedException thrown = ExpectedException.none();
 
 	@Test
-	public void mapaSeCreaPorDefaultConTamanio16() throws ErrorBasico {
+	public void mapaSeCreaPorDefaultConTamanio16() throws Excepcion {
 		Tablero unTablero = new Tablero();
 		Assert.assertNotNull(unTablero.obtenerCasillaEn(15, 15));
 		Assert.assertNotNull(unTablero.obtenerCasillaEn(0, 0));
 		
-		thrown.expect(ErrorBasico.class);
+		thrown.expect(Excepcion.class);
 		unTablero.obtenerCasillaEn(16, 16);
 	}
 	
 	@Test
-	public void mapaSeCreaConTamanioIndicado() throws ErrorBasico {
+	public void mapaSeCreaConTamanioIndicado() throws Excepcion {
 		//Desde (0,0) a (4,4), un tablero de 5x5
 		Tablero unTablero = new Tablero(5,5);
 		Assert.assertNotNull(unTablero.obtenerCasillaEn(4, 4));
 		Assert.assertNotNull(unTablero.obtenerCasillaEn(0, 0));
 		
-		thrown.expect(ErrorBasico.class);
+		thrown.expect(Excepcion.class);
 		unTablero.obtenerCasillaEn(5, 5);
 	}
 	
 	@Test
-	public void noPuedoDefinirAreaFueraDeRango() throws ErrorBasico {
+	public void noPuedoDefinirAreaFueraDeRango() throws Excepcion {
 		Tablero unTablero = new Tablero();
 		
-		thrown.expect(ErrorBasico.class);
+		thrown.expect(Excepcion.class);
 		Area area = unTablero.definirArea(0, 0, 16, 16);
 		Assert.assertNull(area);
 	}
 	
 	@Test
-	public void noSePuedeMoverUnidadesFueraDelMapa() throws ErrorBasico {
+	public void noSePuedeMoverUnidadesFueraDelMapa() throws Excepcion {
 		//16x16
 		Tablero unTablero = new Tablero();
 		Area espacioBorde = unTablero.definirArea(15,15,15,15);
@@ -50,7 +50,7 @@ public class TableroTest {
 		Assert.assertNotNull(espacioBorde);
 		Assert.assertNotNull(unAldeano);
 		
-		thrown.expect(ErrorBasico.class);
+		thrown.expect(Excepcion.class);
 		unTablero.moverDerecha(unAldeano);
 	}
 	
