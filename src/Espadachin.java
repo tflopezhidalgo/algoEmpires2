bbff984c167f826350040f7047957dbffdf3e663
IvoBiaus <43274372+IvoBiaus@users.noreply.github.com>
2018-11-18 16:30:28 -0300

    Update Espadachin.java

--- src/Espadachin.java
+++ src/Espadachin.java
@@ -7,19 +7,30 @@ public class Espadachin extends Unidad {
      *      Costo: 50
      */
 
-	Espadachin(Area unArea) throws Excepcion {
-		super(unArea);
+	Espadachin(Area unEspacio) throws Excepcion {
+		super(unEspacio);
 		vida = 100;
 		costo = 50;
 	}
 
-	public void atacar(Pieza piezaEnemiga) throws Excepcion{
+	public void atacar(Edificio edificioEnemigo) throws Excepcion {
+		if(enRango(edificioEnemigo,1)) {
+			edificioEnemigo.recibirDanio(15);
+		}
+		
+		if(edificioEnemigo.estaDestruida()) {
+			edificioEnemigo = null;
+		}
+	}
+	
+	public void atacar(Unidad unidadEnemiga) throws Excepcion {
+		if(enRango(unidadEnemiga,1)) {
+			unidadEnemiga.recibirDanio(25);
+		}
+		
+		if(unidadEnemiga.estaDestruida()) {
+			unidadEnemiga = null;
+		}
+	}
 
-	    enRango(piezaEnemiga, 1);
-	    piezaEnemiga.recibirDanio(15);
-
-	    if(piezaEnemiga.estaDestruida())
-
-	        piezaEnemiga.obtenerAreaOcupada().liberarCasillas();
-    }
 }
