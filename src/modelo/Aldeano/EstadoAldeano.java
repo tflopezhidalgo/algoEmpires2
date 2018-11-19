8ea6b702571ea0b41a280158452e8d40a948e1b2
IvoBiaus <43274372+IvoBiaus@users.noreply.github.com>
2018-11-18 21:26:01 -0300

    Add files via upload

---
+++ src/modelo/Aldeano/EstadoAldeano.java
@@ -0,0 +1,12 @@
+
+public interface EstadoAldeano {
+
+	public EstadoAldeano reparar(Edificio unEdificio) throws Excepcion;
+	
+	public Plaza crearPlaza(Area areaDeConstruccion) throws Excepcion;
+	
+	public Cuartel crearCuartel(Area areaDeConstruccion) throws Excepcion;
+	
+	public EstadoAldeano realizarTrabajoDeTurno(Edificio edificioObjetivo) throws Excepcion;
+		
+}
